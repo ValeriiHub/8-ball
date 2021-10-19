4efe3ba845b501769cf09b0235fd157733a914c0
Valerii <v.du@dcjudgement.com>
2021-10-19 22:29:10 +0300

    Add networking and shake methods

--- 8-ball/Controllers/ViewController.swift
+++ 8-ball/Controllers/ViewController.swift
@@ -12,15 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var askButton: UIButton!
     
+    let networking = Networking.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        askButton.layer.cornerRadius = 15
     }
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        
+        networking.fetchAnswer { answer in
+            
+            DispatchQueue.main.async {
+                self.answerLabel.text = answer.magic.answer
+            }
+        }
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            networking.fetchAnswer { answer in
+                
+                DispatchQueue.main.async {
+                    self.answerLabel.text = answer.magic.answer
+                }
+            }
+        }
+    }
 }
 
