8fa8e861870226cb6a85f1622bf5db6af0f73462
Valerii <v.du@dcjudgement.com>
2021-10-19 20:56:30 +0300

    Initial Commit

---
+++ 8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8-ball
+//
+//  Created by Valerii Dulia on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
